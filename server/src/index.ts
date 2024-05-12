bc8506eaa18571267a2568aebea5d64719a6ee74
Wei Kian <weikian92@hotmail.com>
2024-05-12 21:41:57 +0800

    setup mongodb, add post request

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,24 @@
-import express from "express";
+import { config } from "dotenv";
+config();
+
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import DeckModel from "./models/Deck";
+
+
+const PORT = 5123;
 const app = express();
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!')
+app.use(express.json());
+app.post('/decks', async (req: Request, res: Response) => {
+    const newDeck = new DeckModel({
+        title: req.body.title
+    });
+    const createDeck =  await newDeck.save();
+    res.json(createDeck);
 });
 
-  
-app.listen(5123);
+mongoose.connect(process.env.MONGO_URL ?? "").then(() => {
+    console.log(`listening on port ${PORT}`);
+    app.listen(PORT);
+});
